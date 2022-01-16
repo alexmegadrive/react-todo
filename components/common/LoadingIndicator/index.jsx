881a7aa0646c4824cc097095ad259274f7634392
Alexander Mihailov <53320300+alexmegadrive@users.noreply.github.com>
2022-01-16 23:33:23 +0500

    Delete components directory

--- components/common/LoadingIndicator/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import styles from './LoadingIndicator.module.css';
-
-const LoadingIndicator = () => {
-    return <div className={styles.loadingIndicator} />;
-}
-
-export default LoadingIndicator
